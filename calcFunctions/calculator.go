36fad0414c6b893e28e035abafb7d847ee0f60ee
mac <eniworoeva@yahoo.com>
2022-07-09 14:21:10 +0100

    refactored the switch function

--- calcFunctions/calculator.go
+++ calcFunctions/calculator.go
@@ -6,7 +6,7 @@ import (
 )
 
 func StringCalculator(arg ...string) []float64 {
-	var resultArr = make([]float64, len(arg), len(arg))
+	var resultArr = make([]float64, len(arg))
 
 	for i := 0; i < len(arg); i++ {
 
@@ -15,12 +15,7 @@ func StringCalculator(arg ...string) []float64 {
 
 			{
 				arrayStr := strings.Split(arg[i], "*")
-				var floatedArr []float64
-				for i := 0; i < len(arrayStr); i++ {
-					newNumbers, _ := strconv.ParseFloat(arrayStr[i], 64)
-					floatedArr = append(floatedArr, newNumbers)
-				}
-
+				floatedArr := changeStringToSlice(arrayStr)
 				resultArr[i] = Multiply(floatedArr)
 				break
 			}
@@ -29,40 +24,22 @@ func StringCalculator(arg ...string) []float64 {
 
 			{
 				arrayStr := strings.Split(arg[i], "+")
-				var floatedArr []float64
-				for i := 0; i < len(arrayStr); i++ {
-					newNumbers, _ := strconv.ParseFloat(arrayStr[i], 64)
-					floatedArr = append(floatedArr, newNumbers)
-				}
-
+				floatedArr := changeStringToSlice(arrayStr)
 				resultArr[i] = Addition(floatedArr)
 				break
-
+			}
 		case strings.Contains(arg[i], "/"):
 
-
-
-				arrayStr := strings.Split(arg[i], "/")
-				var floatedArr []float64
-				for i := 0; i < len(arrayStr); i++ {
-					newNumbers, _ := strconv.ParseFloat(arrayStr[i], 64)
-					floatedArr = append(floatedArr, newNumbers)
-				}
-
-				resultArr[i] = Division(floatedArr)
-				fallthrough
-
+			arrayStr := strings.Split(arg[i], "/")
+			floatedArr := changeStringToSlice(arrayStr)
+			resultArr[i] = Division(floatedArr)
+			fallthrough
 
 		case strings.Contains(arg[i], "-"):
 
 			{
 				arrayStr := strings.Split(arg[i], "-")
-				var floatedArr []float64
-				for i := 0; i < len(arrayStr); i++ {
-					newNumbers, _ := strconv.ParseFloat(arrayStr[i], 64)
-					floatedArr = append(floatedArr, newNumbers)
-				}
-
+				floatedArr := changeStringToSlice(arrayStr)
 				resultArr[i] = Subtraction(floatedArr)
 				break
 			}
@@ -72,11 +49,12 @@ func StringCalculator(arg ...string) []float64 {
 	return resultArr
 }
 
-//func changeStringToSlice(s []string) []float64  {
-//	var floatedArr []float64
-//	for i := 0; i < len(s); i++ {
-//		newNumbers, _ := strconv.ParseFloat(s[i], 64)
-//		floatedArr = append(floatedArr, newNumbers)
-//	}
-//	return floatedArr
-//}
+//function converts slices of string into slices of floats
+func changeStringToSlice(s []string) []float64 {
+	var floatedArr []float64
+	for i := 0; i < len(s); i++ {
+		newNumbers, _ := strconv.ParseFloat(s[i], 64)
+		floatedArr = append(floatedArr, newNumbers)
+	}
+	return floatedArr
+}
